bdc754a006e000e6437b704312364947616ef750
Gan Kang <44344785+bbyiinini@users.noreply.github.com>
2020-10-25 20:57:52 -0700

    Initial commit
    Add Login page and router for this web app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login.js'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          {/* <NavBar
+            loggedIn={this.state.loggedIn}
+            toggleLogin={this.toggleLogin}
+          /> */}
+          <Switch>
+            <Route exact path="/">
+              <div className="root">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="/login"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Login
+                  </a>
+                </header>
+              </div>
+            </Route>
+            <Route exact path="/login" component={() => <Login />} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
+
