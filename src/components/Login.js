6d57eff7c2b56ed700489bc02252b94187f0c310
Gan Kang <44344785+bbyiinini@users.noreply.github.com>
2020-10-25 21:48:16 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,50 @@
-import React, { Component } from "react"
+import React, { useState }  from "react";
+import { useHistory } from "react-router-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
-  }
- }
-render() {
+
+const Login = (props) => {
+
+    const [password, setPassword] = useState("");
+    const [Id, setId] = useState("");
+    const [signedIn, setSignedIn] = useState(false);
+    const history = useHistory();
+
+    const passChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const IdChange = (e) => {
+        setId(e.target.value);
+    };
+
     return (
-      <div>
-        <MuiThemeProvider>
-          <div>
-          <AppBar
-             title="Login"
-           />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      </div>
+        <div>
+            <MuiThemeProvider>
+            <div>
+                <AppBar title="Login"/>
+                <TextField
+                    hintText="Enter your Username"
+                    floatingLabelText="Username"
+                    onChange = {IdChange}
+                    />
+                <br/>
+                    <TextField
+                    type="password"
+                    hintText="Enter your Password"
+                    floatingLabelText="Password"
+                    onChange = {passChange}
+                    />
+                <br/>
+                <RaisedButton label="Submit" primary={true} style={style} onClick={""}/>
+            </div>
+            </MuiThemeProvider>
+        </div>
     );
-  }
 }
-const style = {
- margin: 15,
-};
+    const style = {margin: 15,};
+
+
 export default Login;
