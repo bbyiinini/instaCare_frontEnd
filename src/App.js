7c6214a3e5b313e558553a7b1f874c88815cf418
Gan Kang <44344785+bbyiinini@users.noreply.github.com>
2020-10-25 21:14:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,14 +26,7 @@ class App extends Component {
                   <p>
                     Edit <code>src/App.js</code> and save to reload.
                   </p>
-                  <a
-                    className="App-link"
-                    href="/login"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Login
-                  </a>
+                  <a href="/login">Login</a>
                 </header>
               </div>
             </Route>
