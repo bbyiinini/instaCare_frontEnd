1d6b2cbd6e7ce314ed044cfdd28d2ced182f0a85
Tianpeng Fan <tif005@ucsd.edu>
2020-11-06 19:29:01 -0800

    add forgot password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect }  from "react";
-import { useHistory } from "react-router-dom";
 import db, {provider1}from '../base';
 import {toast} from "react-toastify";
-import {useSelector} from "react-redux";
+
+
+
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState("")
 
-    const handleSubmit = () => {
+    const handleSubmit = async e => {
+        e.preventDefault();
 
+        const actionSetting = {
+            url: 'http://localhost:3000/signup/complete',
+            handleCodeInApp: true,
+        }
+        // console.log(email)
+        await db.auth().sendPasswordResetEmail(email, actionSetting)
+        toast.success(`Email is sent to ${email}, please confirm your email and continue to complete PasswordReset`);
     }
 
     return (
         <div className="container col-md-6 offset-md-3 p-5">
             <h4>Forgot Password</h4>
             <form onSubmit={handleSubmit}>
-                    <input type="email" style={style} className="form-control" value={email} onChange={e=>setEmail(e.target.value)}
-                           placeholder="Enter your email"
-                           autoFocus
-                    />
+                <input type="email" style={style} className="form-control" value={email} onChange={e=>setEmail(e.target.value)}
+                       placeholder="Enter your email"
+                       autoFocus
+                />
                 <br/>
-                <button className="btn btn-primary" disabled={!email}>Change Password</button>
+                <button className="btn btn-primary" onClick={handleSubmit} >Change Password</button>
             </form>
         </div>
 
@@ -29,3 +38,4 @@ const ForgotPassword = () => {
 
 const style = {width: "300px", margin:"0 auto"}
 export default ForgotPassword;
+
